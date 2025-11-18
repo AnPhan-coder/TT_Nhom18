6f9fe771ed1e237d225f20a70079802015a524d3
Phan Ng Hoai An <pandora223311@gmail.com>
2025-11-18 10:13:27 +0700

    init backend

---
+++ ac_spring/src/main/java/com/ac/AnCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.ac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnCinemaApplication.class, args);
+	}
+
+}
